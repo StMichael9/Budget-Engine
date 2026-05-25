731836794123641883dedfd37cdfcc0c68f2ee05
St Michael Egenamba <michaelegenamba@gmail.com>
2026-05-25 00:58:29 -0700

    Working on reading budgets.json

--- budget_engine/analyzer.py
+++ budget_engine/analyzer.py
@@ -16,10 +16,30 @@ This is the “math brain.”"""
 with open ('data/transactions.json', 'r') as file:
     data = json.load(file)
 
+    # Open the file and parse JSON content
+with open ('data/budgets.json', 'r') as file:
+    data_budgets = json.load(file)
+
+
+# this needs to track of totals per catagory
+totals = {}
+
 for item in data:
     amount = item["amount"]
     merchant = item["merchant"]
+    category = item["category"]
     print(f"Spent {amount} on {merchant}")
+
+    if category in totals:
+        totals[category] += amount
+    else:
+        totals[category] = amount
+
+for category, total in totals.items():
+    print(f"{category}: ${total:.2f}")
+
+
+
 
 
 #for d in data:
