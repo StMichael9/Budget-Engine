e50df9e2ee2059a265236e98298c84fec761fc23
St Michael Egenamba <michaelegenamba@gmail.com>
2026-05-28 21:44:46 -0700

    I was wrong v1 is not finished i still need to format and clean data

--- budget_engine/formatter.py
+++ budget_engine/formatter.py
@@ -5,3 +5,17 @@ turning results into JSON
 turning results into pretty console output
 
 preparing data for future API responses"""
+
+import json
+
+def to_json(data):
+    # convert Python dict to JSON string
+    pass
+
+def to_pretty_console(data):
+    # convert Python dict to formatted console output
+    pass
+
+def to_api_response(data):
+    # prepare standardized API response dict
+    pass
